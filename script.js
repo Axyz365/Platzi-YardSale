145f7e99114d025028824c8f6f7bd135854ea14b
Azyx365 <aleex.365x@gmailcom>
2022-11-19 14:19:49 -0600

    adding menu mobile

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const arrowMenu = document.querySelector('.arrow-menu');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 arrowMenu.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toogleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
