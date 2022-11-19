fd0814c8b1233a4d810702b1ff4281e979af57b7
Azyx365 <aleex.365x@gmailcom>
2022-11-19 16:50:45 -0600

    adding cart and fixing menus

--- script.js
+++ script.js
@@ -2,15 +2,42 @@ const arrowMenu = document.querySelector('.arrow-menu');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.burger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cart = document.querySelector('.navbar-shopping-cart');
+const cartDetails = document.querySelector('.product-detail');
 
-
+cart.addEventListener('click', toogleCart);
 arrowMenu.addEventListener('click', toggleDesktopMenu);
 burgerMenu.addEventListener('click', toogleMobileMenu)
 
+// FUNCTIONS
 function toggleDesktopMenu(){
+
+    let isCartClosed = cartDetails.classList.contains('inactive');
+
+    if(!isCartClosed) {
+        cartDetails.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
+
 }
 
 function toogleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+
+    cartDetails.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive');
+
+}
+
+function toogleCart(){
+
+    let isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    mobileMenu.classList.add('inactive');
+    cartDetails.classList.toggle('inactive');
+
 }
